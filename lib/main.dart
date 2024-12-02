3c1b0ca0497c73b3f0dcc7eb28f69be7d30a92b2
Tauseef Ahamed <tauseefahamed14@gmail.com>
2024-12-02 22:29:40 +0530

    feat: Project Initialization

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:news_app/core/routes/route_provider.dart';
+import 'package:news_app/utils/theme.dart';
+
+void main() {
+  runApp(const ProviderScope(
+    child: MainWidget(),
+  ));
+}
+
+class MainWidget extends ConsumerWidget {
+  const MainWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routeProvider);
+    return MaterialApp.router(
+      title: 'SimpleNews',
+      routerConfig: router,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: NewsTheme.lightTheme,
+      darkTheme: NewsTheme.darkTheme,
+    );
+  }
+}
